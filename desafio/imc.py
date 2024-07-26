64ad380ece8d6c8b12080e41f270b3820bd8286d
Carlos Silva Diaz <crasd15@live.com>
2024-07-26 11:55:03 -0400

    calcular imc
    se agrega la funcion para calcular el imc

--- desafio/imc.py
+++ desafio/imc.py
@@ -1,3 +1,22 @@
+def calcular_imc(peso,altura):
+    """
+    Calcular el indice de masa corporal (IMC)
+
+    Parametros: 
+    peso (float): Peso de la persona en kilometros
+    altura (float): Altura de la persona en metros
+
+    Retorna: 
+    float: El valor del IMC
+    """
+
+    imc = peso / (altura ** 2)
+    return imc
+
+
+
+
+
 def main():
 
     """
