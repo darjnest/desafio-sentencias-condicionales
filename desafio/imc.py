94136ab2cb9e7e01267d4f428b667228bd3659dd
Carlos Silva Diaz <crasd15@live.com>
2024-07-26 11:51:35 -0400

    primer comentario
    se agrega la entrada de datos a la app

---
+++ desafio/imc.py
@@ -0,0 +1,10 @@
+def main():
+
+    """
+    Funcion principal que ejecuta el programa 
+    Solicita al usuario que ingrese su peso y altura , calcula el imc y muestra la clasificacion correspondiente.
+    """
+
+    peso = input(float("Ingrese su peso en kilogramos: "))
+
+    altura = input(float("Ingrese su altura en metro: "))
